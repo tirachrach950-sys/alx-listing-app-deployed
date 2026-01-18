fede7834af7a8d016305c69e486f0e1e9bdcbe3e
tirachrach950-sys <tirachrach950@gmail.com>
2026-01-18 20:34:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>NEXUS</h1>
+        <p>Your milestone is running!</p>
+      </header>
+      <main>
+        <p>Front-End project built with React + Vite</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
 
